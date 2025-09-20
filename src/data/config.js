5c57030dc85bd5a4d1cb433ca0e16810a7e044f0
nodeUpdate <tes@mail.com>
2025-09-21 03:58:48 +0700

    latest cron update

--- src/data/config.js
+++ src/data/config.js
@@ -1,2 +1,2 @@
-export const version = "3.35";
+export const version = "3.36";
 export const domain = "https://nqapp.nurulquran.com"
--- src/service-worker.js
+++ src/service-worker.js
@@ -21,7 +21,7 @@ clientsClaim();
 
 setCacheNameDetails({
     prefix: "nurulquran",
-    suffix: "3.4",
+    suffix: "3.5",
     precache: "nurulquran-precache",
     runtime: "nurulquran-runtime",
 });
