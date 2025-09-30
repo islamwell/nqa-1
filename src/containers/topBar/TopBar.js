a0de8257060adca39d5e711aa7ab09751736266b
Ridho Anugrah <im.ridgra@gmail.com>
2025-09-30 08:35:57 +0700

    Add version check for app updates and trigger audio list download

--- src/containers/topBar/TopBar.js
+++ src/containers/topBar/TopBar.js
@@ -173,6 +173,22 @@ export default function PrimarySearchAppBar() {
   };
 
   useEffect(() => {
+    const storedVersion = localStorage.getItem('app_version');
+    const compareVersions = (v1, v2) => {
+      const parts1 = v1.split('.').map(Number);
+      const parts2 = v2.split('.').map(Number);
+      for (let i = 0; i < Math.max(parts1.length, parts2.length); i++) {
+        const p1 = parts1[i] || 0;
+        const p2 = parts2[i] || 0;
+        if (p1 > p2) return 1;
+        if (p1 < p2) return -1;
+      }
+      return 0;
+    };
+    if (!storedVersion || compareVersions(version, storedVersion) > 0) {
+      localStorage.setItem('app_version', version);
+      dispatch(downloadAudioList());
+    }
     handleOffline()
   }, [])
 
